da43ed73f9b9289f786190e07a3b435fd1c0632e
Christian Stein <sormuras@gmail.com>
2023-03-14 09:07:25 +0100

    Include Java source files in archive

--- src/duke.run/Build.java
+++ src/duke.run/Build.java
@@ -2,97 +2,111 @@ import static java.lang.System.err;
 import static java.lang.System.out;
 
 import java.lang.module.ModuleDescriptor.Version;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Map;
 import java.util.spi.ToolProvider;
 
-/**
- * <pre>{@code
- *   DUKE_ARCHIVE: 'run.duke@early-access.jar'
- *   DUKE_VERSION: '0-ea'
- *   DUKE_VERSION_BUILD: ${{ github.sha }}
- * }</pre>
- */
+/** Duke's build program. */
 class Build {
   public static void main(String... args) {
+    var configuration = Configuration.of(System.getenv());
+    out.println("Build Duke " + configuration.version());
 
-    var version = Version.parse(args.length == 0 ? "0-ea+development" : args[0]);
-    out.println("Build Duke " + version);
-    compileJavaModuleRunDuke();
-    createJavaArchiveRunDuke();
-    deleteCompiledClasses();
+    var builder = new Builder(configuration);
+    builder.compileJavaModuleRunDuke();
+    builder.createJavaArchiveRunDuke();
   }
 
-  static void compileJavaModuleRunDuke() {
-    run(
-        "javac",
-        "--release=17",
-        "--module=run.duke",
-        "--module-source-path=run.duke=src/run.duke/main/java",
-        "-d",
-        ".duke/lib");
-  }
+  /**
+   * Build settings.
+   *
+   * @param archive {@code run.duke@development.jar}
+   * @param version {@code 0-dev}
+   */
+  record Configuration(Version version, String archive, Folders folders) {
+    record Folders(Path root, Path duke, Path lib, Path tmp) {
+      static Folders ofCurrentWorkingDirectory() {
+        return Folders.of(Path.of(""));
+      }
 
-  static void createJavaArchiveRunDuke() {
-    var version = moduleVersionWithBuildInformation();
-    var archive = System.getenv().getOrDefault("DUKE_ARCHIVE", "run.duke@" + version + ".jar");
-    run(
-        "jar",
-        "--create",
-        "--file=.duke/lib/" + archive,
-        "--module-version=" + version,
-        "--main-class=run.duke.Main",
-        "-C",
-        ".duke/lib/run.duke",
-        ".");
-  }
-
-  static void deleteCompiledClasses() {
-    delete(Path.of(".duke/lib/run.duke"));
-  }
-
-  static void delete(Path path) {
-    var start = path.normalize().toAbsolutePath();
-    if (Files.notExists(start)) return;
-    for (var root : start.getFileSystem().getRootDirectories()) {
-      if (start.equals(root)) {
-        return;
+      static Folders of(Path root) {
+        var duke = root.resolve(".duke");
+        return new Folders(root, duke, duke.resolve("lib"), duke.resolve("tmp"));
       }
     }
-    try (var stream = Files.walk(start)) {
-      var files = stream.sorted((p, q) -> -p.compareTo(q));
-      for (var file : files.toArray(Path[]::new)) Files.deleteIfExists(file);
-    } catch (Exception exception) {
-      throw new RuntimeException(exception);
+
+    static final Configuration DEFAULT =
+        new Configuration(
+            Version.parse("0-dev"),
+            "run.duke@development.jar",
+            Folders.ofCurrentWorkingDirectory());
+
+    static Configuration of(Map<String, String> map) {
+      var version = configureModuleVersionWithBuildInformation(map);
+      var archive = map.getOrDefault("DUKE_ARCHIVE", DEFAULT.archive());
+      var folders = Folders.ofCurrentWorkingDirectory();
+      return new Configuration(version, archive, folders);
+    }
+
+    static Version configureModuleVersionWithBuildInformation(Map<String, String> map) {
+      var version = map.getOrDefault("DUKE_VERSION", DEFAULT.version().toString());
+      var build = new StringBuilder();
+      var now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+      build.append(now.format(DateTimeFormatter.BASIC_ISO_DATE));
+      build.append('-');
+      var sha = System.getenv("GITHUB_SHA");
+      if (sha != null) build.append(sha, 0, Math.min(7, sha.length()));
+      else build.append(now.format(DateTimeFormatter.ofPattern("HHmmss")));
+      return Version.parse(version + "+" + build);
     }
   }
 
-  static void run(String tool, Object... arguments) {
+  /**
+   * Build helper.
+   *
+   * @param configuration build settings
+   */
+  record Builder(Configuration configuration) {
+    void compileJavaModuleRunDuke() {
+      run(
+          "javac",
+          "--release=17",
+          "--module=run.duke",
+          "--module-source-path=run.duke=src/run.duke/main/java",
+          "-d",
+          configuration.folders().tmp());
+    }
+
+    void createJavaArchiveRunDuke() {
+      run(
+          "jar",
+          "--create",
+          "--file=" + configuration.folders().lib().resolve(configuration.archive()),
+          "--module-version=" + configuration.version(),
+          "--main-class=run.duke.Main",
+          "-C",
+          configuration.folders().tmp().resolve("run.duke"),
+          ".",
+          "-C",
+          configuration.folders().root().resolve("src/run.duke/main/java"),
+          ".");
+    }
+  }
+
+  private static void run(String tool, Object... arguments) {
     run(tool, List.of(arguments));
   }
 
-  static void run(String tool, List<Object> arguments) {
+  private static void run(String tool, List<Object> arguments) {
     var provider = ToolProvider.findFirst(tool).orElseThrow();
     var args = arguments.stream().map(Object::toString).toArray(String[]::new);
     out.printf("| + %s %s%n", tool, String.join(" ", args));
     var code = provider.run(out, err, args);
     if (code == 0) return;
     throw new AssertionError("Tool " + tool + " failed with: " + code);
-  }
-
-  static Version moduleVersionWithBuildInformation() {
-    var version = System.getenv().getOrDefault("DUKE_VERSION", "0-dev");
-    var build = new StringBuilder();
-    var now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-    build.append(now.format(DateTimeFormatter.BASIC_ISO_DATE));
-    build.append('-');
-    var sha = System.getenv("GITHUB_SHA");
-    if (sha != null) build.append(sha, 0, Math.min(7, sha.length()));
-    else build.append(now.format(DateTimeFormatter.ofPattern("HHmmss")));
-    return Version.parse(version + "+" + build);
   }
 }
